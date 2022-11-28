1e7f393489ff941edf73d036c0adff058db8fa3e
ldm0805 <leonardodimaggio94@gmail.com>
2022-11-28 18:37:00 +0100

    Modifica biglietto con i dati

--- js/script.js
+++ js/script.js
@@ -3,53 +3,50 @@
 // Chiedere all'utente di inserire il numero di chilometri che vuole percorrere;
 // let distance = parseInt(prompt('Inserisci i chilometri che vuoi percorrere'));
 
-let button_data_name = document.getElementById('save_data');
-let button_data_distance = parseInt(document.getElementById('dave_data'));
-let button_data_age = parseInt(document.getElementById('save_data'));
- 
-save_data.addEventListener('click', function(){
-    let input_tag = document.getElementById('nome');
-    let value_input = input_tag.value;
-    console.log(value_input);
-})
+let button = document.getElementById('save_data');
 
-save_data.addEventListener('click', function(){
-    let input_tag = document.getElementById('distance_km');
-    let value_input = input_tag.value;
-    console.log(value_input);
-})
-
-save_data.addEventListener('click', function(){
-    let input_tag = document.getElementById('age_customer');
-    let value_input = input_tag.value;
-    console.log(value_input);
-})
-
-
-// Calcolare il prezzo del biglietto in base al prezzo per chilometro (0.21 € al km) e tenendo conto dell'eventuale scontistica nel caso l'età dell'utente soddisfi uno dei seguenti requisiti:
-const price_km = 0.21;
-let part_price = (button_data_distance * price_km);
+let part_price
+let name_tag
+let distance_tag
+let age_tag
 let final_price
-// SE L'utente è minorenne
-//         ALLORA ha diritto ad uno sconto del 20%;
-if (button_data_age < 18){
-    final_price = (part_price - (part_price * 0.2).toFixed(2));
-    console.log(final_price)
-}
+button.addEventListener('click', function(){
+    name_tag = document.getElementById('nome').value;
+    console.log(name_tag)
+    
+    distance_tag = document.getElementById('distance_km').value;
+    console.log(distance_tag)
 
-// SE L'utente ha più di 65 anni 
-//         ALLORA ha diritto ad uno sconto del 40%; 
-else if (button_data_age >= 65){
-    final_price = (part_price - (part_price * 0.4)).toFixed(2);
-    console.log(final_price)
+    age_tag = document.getElementById('age_customer').value;
+    console.log(age_tag)
 
-}
+    const price_km = 0.21;
+    part_price = (distance_tag * price_km);
+    console.log(part_price);
 
-// SE L'utente è maggiorenne ma ha meno di 65 anni 
-//         ALLORA non ha diritto allo sconto;
-else {
-    final_price = part_price.toFixed(2);
-    console.log(final_price)
+    if (age_tag < 18){
+        final_price = (part_price - (part_price * 0.2).toFixed(2));
+        console.log(final_price);
+    }
+    
+    else if (age_tag >= 65){
+        final_price = (part_price - (part_price * 0.4)).toFixed(2);
+        console.log(final_price);
+    }
 
-}
+    else{
+        final_price = part_price.toFixed(2);
+    }
+
+    document.getElementById('nome-pass').innerHTML ='ciao'+ " " + name_tag;
+    document.getElementById('nome-pass').innerHTML ='ciao'+ " " + name_tag;
+    document.getElementById('nome-pass').innerHTML ='ciao'+ " " + name_tag;
+})
+
+
+
+
+
+
+
 
