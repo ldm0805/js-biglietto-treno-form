7ac4f7bcf8d0fec5b644e886df13f6f51bafcebd
ldm0805 <leonardodimaggio94@gmail.com>
2022-11-28 16:29:50 +0100

    Ultima versione

--- js/script.js
+++ js/script.js
@@ -24,32 +24,33 @@ button_data_name.addEventListener('click', function(){
     let value_input = input_tag.value;
     console.log(value_input);
 })
-// Chiedere all'utente di inserire la sua età;
-// // let age = parseInt(prompt('Inserisci la tua età'));
 
-// // Calcolare il prezzo del biglietto in base al prezzo per chilometro (0.21 € al km) e tenendo conto dell'eventuale scontistica nel caso l'età dell'utente soddisfi uno dei seguenti requisiti:
-// const price_km = 0.21;
-// // let part_price = (distance * price_km);
 
-// let final_price
-// // SE L'utente è minorenne
-// //         ALLORA ha diritto ad uno sconto del 20%;
-// if (age < 18){
-//     final_price = (part_price - (part_price * 0.2).toFixed(2));
-// }
+// Calcolare il prezzo del biglietto in base al prezzo per chilometro (0.21 € al km) e tenendo conto dell'eventuale scontistica nel caso l'età dell'utente soddisfi uno dei seguenti requisiti:
+const price_km = 0.21;
+let part_price = (button_data_distance * price_km);
 
-// // SE L'utente ha più di 65 anni 
-// //         ALLORA ha diritto ad uno sconto del 40%; 
-// else if (age >= 65){
-//     final_price = (part_price - (part_price * 0.4)).toFixed(2);
-// }
+let final_price
+// SE L'utente è minorenne
+//         ALLORA ha diritto ad uno sconto del 20%;
+if (button_data_age < 18){
+    final_price = (part_price - (part_price * 0.2).toFixed(2));
+    console.log(final_price)
+}
 
-// // SE L'utente è maggiorenne ma ha meno di 65 anni 
-// //         ALLORA non ha diritto allo sconto;
-// else{
-//     final_price = part_price.toFixed(2);
-// }
+// SE L'utente ha più di 65 anni 
+//         ALLORA ha diritto ad uno sconto del 40%; 
+else if (button_data_age >= 65){
+    final_price = (part_price - (part_price * 0.4)).toFixed(2);
+    console.log(final_price)
 
-// // Restituire a schermo la password.
-// document.getElementById('client_price').innerHTML = 'Il prezzo del biglietto è di:'+ " " + final_price.toFixed(2) +" "+ "&euro;";
+}
+
+// SE L'utente è maggiorenne ma ha meno di 65 anni 
+//         ALLORA non ha diritto allo sconto;
+else {
+    final_price = part_price.toFixed(2);
+    console.log(final_price)
+
+}
 
