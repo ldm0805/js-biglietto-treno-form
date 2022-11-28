af7563d46638b07718c243790729d635d8fa3c6c
ldm0805 <leonardodimaggio94@gmail.com>
2022-11-28 17:13:49 +0100

    ultimo push

--- js/script.js
+++ js/script.js
@@ -3,23 +3,23 @@
 // Chiedere all'utente di inserire il numero di chilometri che vuole percorrere;
 // let distance = parseInt(prompt('Inserisci i chilometri che vuoi percorrere'));
 
-const button_data_name = document.getElementById('save_data');
-const button_data_distance = document.getElementById('dave_data');
-const button_data_age = document.getElementById('save_data');
+let button_data_name = document.getElementById('save_data');
+let button_data_distance = parseInt(document.getElementById('dave_data'));
+let button_data_age = parseInt(document.getElementById('save_data'));
  
-button_data_name.addEventListener('click', function(){
+save_data.addEventListener('click', function(){
     let input_tag = document.getElementById('nome');
     let value_input = input_tag.value;
     console.log(value_input);
 })
 
-button_data_name.addEventListener('click', function(){
+save_data.addEventListener('click', function(){
     let input_tag = document.getElementById('distance_km');
     let value_input = input_tag.value;
     console.log(value_input);
 })
 
-button_data_name.addEventListener('click', function(){
+save_data.addEventListener('click', function(){
     let input_tag = document.getElementById('age_customer');
     let value_input = input_tag.value;
     console.log(value_input);
@@ -29,7 +29,6 @@ button_data_name.addEventListener('click', function(){
 // Calcolare il prezzo del biglietto in base al prezzo per chilometro (0.21 € al km) e tenendo conto dell'eventuale scontistica nel caso l'età dell'utente soddisfi uno dei seguenti requisiti:
 const price_km = 0.21;
 let part_price = (button_data_distance * price_km);
-
 let final_price
 // SE L'utente è minorenne
 //         ALLORA ha diritto ad uno sconto del 20%;
